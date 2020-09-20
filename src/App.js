0c089a71da9aeb975b9c35c41dcfc0de377a9904
Brayan Lara <bralavar@gmail.com>
2020-09-20 16:00:44 -0500

    configuration of app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// @flow
+import { hot } from 'react-hot-loader/root'
+import React from 'react'
+
+const App = () => (
+  <div className='app'>
+    <h1> Hello, World!</h1>
+  </div>
+)
+
+export default hot(App)
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    port: 3000,
+    open: true,
+    hotOnly: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
